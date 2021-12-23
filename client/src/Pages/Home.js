124da3a2f8df6477f81d8efc419cd206e0c47356
mohammedahw <mohammedalihw@gmail.com>
2021-12-23 18:46:29 +0300

    added navbar

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,16 +1,51 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
 import axios from "axios";
-import { useState, useEffect, useReducer } from "react";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  loading: true,
+  error: "",
+  books: {},
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_SUCCESS":
+      return {
+        loading: false,
+        error: "",
+        books: action.payload,
+      };
+    case "FETCH_ERROR":
+      return {
+        loading: true,
+        error: "gg",
+        books: {},
+      };
+    default:
+      return state;
+  }
+};
 
 export const Home = () => {
-  const [data, setData] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
-    axios.get("localhost:5000/api/books").then((res) => console.log(res));
+    axios
+      .get("http://localhost:5000/api/books")
+      .then((response) => {
+        dispatch({ type: "FETCH_SUCCESS", payload: response.data });
+      })
+      .catch(() => {
+        dispatch({ type: "FETCH_ERROR" });
+      });
   }, []);
+
   return (
     <>
       <Navbar />
+      {state.loading ? "loading..." : "books"}
     </>
   );
 };
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,54 @@
 import React from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <LibraryBooksIcon />
+          </IconButton>
+          <Button sx={{ textTransform: "capitalize" }}>
+            <Link
+              to="/"
+              underline="none"
+              color="white"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Home
+              </Typography>
+            </Link>
+          </Button>
+          <Button sx={{ ml: "auto", textTransform: "capitalize" }}>
+            <Link
+              to="/login"
+              underline="none"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Login
+              </Typography>
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
